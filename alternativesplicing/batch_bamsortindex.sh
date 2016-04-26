6a9de34b02f61bc56051070f43f6ee0e2aae4cef
peter-koo <peter_koo@harvard.edu>
2016-04-26 09:56:27 -0400

    bamsortindex

--- alternativesplicing/batch_bamsortindex.sh
+++ alternativesplicing/batch_bamsortindex.sh
@@ -5,10 +5,10 @@
 #
 #SBATCH -J BamSortIndex					# job name
 #SBATCH -p serial_requeue        	# partition (general,serial_requeue)
-#SBATCH -n 1                        # number of cores
+#SBATCH -n 2                       # number of cores
 #SBATCH -N 1                            # number of nodes
 #SBATCH --mem 4000                	# memory pool for all cores
-#SBATCH -t 0-3:00                       # time (D-HH:MM)
+#SBATCH -t 0-12:00                       # time (D-HH:MM)
 #SBATCH -o log/sam_%A.out   	# STDOUT
 #SBATCH -e log/sam_%A.err     	# STDERR
 #SBATCH --mail-type=FAIL 		# notifications (END,FAIL,ALL)
--- assembly/batch_stringtie_merge.sh
+++ assembly/batch_stringtie_merge.sh
@@ -8,10 +8,10 @@
 #
 #SBATCH -J Stringtie_merge			# job name
 #SBATCH -p serial_requeue        	# partition (general,serial_requeue)
-#SBATCH -n 12                           # number of cores
+#SBATCH -n 8                           # number of cores
 #SBATCH -N 1                            # number of nodes
 #SBATCH --mem 8000                	# memory pool for all cores
-#SBATCH -t 0-1:30                       # time (D-HH:MM)
+#SBATCH -t 0-1:00                       # time (D-HH:MM)
 #SBATCH -o log/stringtie_merge_%A.out   	# STDOUT
 #SBATCH -e log/stringtie_merge_%A.err     	# STDERR
 #SBATCH --mail-type=FAIL 		# notifications (END,FAIL,ALL)
