8099fa5f285c3d9142167a6d74e4cb908a924c51
peter-koo <peter_koo@harvard.edu>
2016-04-27 17:05:01 -0400

    sra

---
+++ preprocessing/run_fastq_dump.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# filename: run_miso.sh
+# summary: wrapper script to run a batch job for batch_miso_index.sh
+
+# inputs
+DATA="/n/regal/eddy_lab/pkoo/mRNAlocalization/data/rawreads/"
+
+for i in $DATA*.sra; do 
+	sbatch batch_fastq_dump $i
+done
